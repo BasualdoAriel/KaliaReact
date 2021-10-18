2acc30738326d943d40ee545b62223b18dec8ad2
Ariel Basualdo <arielbasualdo2014@outlook.com>
2021-10-18 15:52:36 -0300

    Correción NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import {NavBar} from './components/Navbar/NavBar'
+import { NavBar } from './components/NavBar/NavBar';
 import {ItemListContainer} from './components/ItemListContainer/ItemListContainer'
+
 
 function App() {
   return (
