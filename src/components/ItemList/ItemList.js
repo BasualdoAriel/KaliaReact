2cc19ae37ff193d231cf07dc55eac9d54ddd4676
Ariel Basualdo <arielbasualdo2014@outlook.com>
2021-10-20 16:59:25 -0300

    Promise en ItemListCointainer

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,33 +1,11 @@
 import { Item } from "../Item/Item";
 import { Container, Spinner } from "react-bootstrap";
-import { useEffect, useState } from "react";
 
 
 export function ItemList({items}){
-    const[products, setProducts]=useState(null);
-
-    useEffect(()=>{
-        const task=new Promise((resolve)=>{
-            setTimeout(()=>{
-                resolve(items);
-            },2000);
-        })
-        task.then(
-            (result)=>{
-                setProducts(result);
-            },
-            (err)=>{
-                console.log(err);
-            }
-        ).catch((err)=>{
-            console.log(err);
-        }).finally(()=>{
-            console.log('finalizó carga');
-        })
-    },)
     return(
         <Container className="d-flex justify-content-center pb-4">
-            {products? products.map(product=><Item item={product} key={product.id}/>):<Spinner animation="border" role="status" variant="info"><span className="visually-hidden">Loading...</span></Spinner>}
+            {items? items.map(item=><Item item={item} key={item.id}/>):<Spinner animation="border" role="status" variant="info"><span className="visually-hidden">Loading...</span></Spinner>}
         </Container>
     )
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import { ItemCount } from "../ItemCount/ItemCount";
 import {ItemList} from '../ItemList/ItemList'
+import { useState, useEffect } from "react";
 
 const items=[
     {id:"billetera", price: 730, name:"Billetera sirena", img:"https://basualdoariel.github.io/KaliaAccesorios/images/bille4.jpeg", desc:"Mini billetera sirena con 4 tarjeteros💳, monedero 🪙 y espacio para dinero 💵.", stock: 5},
@@ -10,11 +11,31 @@ const items=[
 
 
 export const ItemListContainer= ()=>{
-    
+    const[products, setProducts]=useState(null);
+
+    useEffect(()=>{
+        const task=new Promise((resolve)=>{
+            setTimeout(()=>{
+                resolve(items);
+            },2000);
+        })
+        task.then(
+            (result)=>{
+                setProducts(result);
+            },
+            (err)=>{
+                console.log(err);
+            }
+        ).catch((err)=>{
+            console.log(err);
+        }).finally(()=>{
+            console.log('finalizó carga');
+        })
+    },)
     return(
         <Container className="text-center">
             <h1 className="pb-4">Kalia Accesorios</h1>
-            <ItemList items={items}/>
+            <ItemList items={products}/>
             <ItemCount  inital="1" stock="5"/>
         </Container>
         
